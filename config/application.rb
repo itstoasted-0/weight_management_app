9bcb687ceee99734596cee1d27eb142d5f4b4734
itstoasted-0 <snow.w686@gmail.com>
2021-04-07 15:06:35 +0900

    Devise　ログイン機能導入、日本語化

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -21,6 +21,11 @@ module WeightManagementApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    # 日本語に変更
+    config.i18n.default_locale = :ja
+    # タイムゾーンも変更
+    config.time_zone = "Asia/Tokyo"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
