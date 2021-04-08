c6a567e05c03fc405b2d71246a039d9b7028ef6b
itstoasted-0 <snow.w686@gmail.com>
2021-04-08 10:53:24 +0900

    過去◯週間ボタンを作成

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -2,13 +2,17 @@ document.addEventListener('turbolinks:load', () => {
     // '2020-01-12'のような文字列から，Javascriptの日付オブジェクトを取得する関数
     // setHoursを使用しないと，時差の影響で0時にならないため注意！
     const convertDate = (date) => new Date(new Date(date).setHours(0, 0, 0, 0))
-
     const TODAY = convertDate(new Date())
     const A_WEEK_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 6)
     const TWO_WEEKS_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 13)
     const A_MONTH_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth() - 1, TODAY.getDate() + 1)
     const THREE_MONTHS_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth() - 3, TODAY.getDate() + 1)
-
+    // 日付の古い方・新しい方を取得する関数
+    const minDate = (date1, date2) => (date1 < date2) ? date1 : date2
+    const maxDate = (date1, date2) => (date1 > date2) ? date1 : date2
+    // データの初日・最終日
+    const START_DATE = convertDate(gon.weight_records[0].date)
+    const END_DATE = convertDate(gon.weight_records[gon.weight_records.length - 1].date)
     // グラフを描く場所を取得
     const chartWeightContext = document.getElementById("chart-weight").getContext('2d')
     // 関数内で変数宣言をするとローカル変数となり，関数の外で消えてしまう
@@ -72,7 +76,30 @@ document.addEventListener('turbolinks:load', () => {
             chartWeight.update()
         }
     }
+    // 引数の日付から今日までのグラフを描く関数
+    const drawGraphToToday = (from) => {
+        // データが存在する範囲に修正
+        from = maxDate(from, START_DATE)
+        let to = minDate(TODAY, END_DATE)
+        drawGraph(from, to)
+    }
 
+    // 過去◯週間のグラフを描くボタン
+    document.getElementById('a-week-button').addEventListener('click', () => {
+        drawGraphToToday(A_WEEK_AGO)
+    })
+
+    document.getElementById('two-weeks-button').addEventListener('click', () => {
+        drawGraphToToday(TWO_WEEKS_AGO)
+    })
+
+    document.getElementById('a-month-button').addEventListener('click', () => {
+        drawGraphToToday(A_MONTH_AGO)
+    })
+
+    document.getElementById('three-months-button').addEventListener('click', () => {
+        drawGraphToToday(THREE_MONTHS_AGO)
+    })
     // グラフの初期表示
-    drawGraph(A_WEEK_AGO, TODAY)
+    drawGraphToday(A_WEEK_AGO)
 })
